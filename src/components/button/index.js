5f66603ee63e808f03eb0871fb536d13886f2175
winsome <winsomeyuen@hotmail.com>
2018-02-26 20:58:21 +0000

    mm

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,6 +1,6 @@
 // import preact
 import { h, render, Component } from 'preact';
-	
+
 export default class Button extends Component {
 
 	// rendering a function when the button is clicked
@@ -9,11 +9,12 @@ export default class Button extends Component {
 			clickFunction = () => {
 				console.log("passed something as 'clickFunction' that wasn't a function !");
 			}
-		}	
+		}
 		return (
 			<div>
+				<input type="text" name="location"></input><br/>
 				<button onClick={clickFunction}>
-					Display Weather
+					Find
 				</button>
 			</div>
 		);
